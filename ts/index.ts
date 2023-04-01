40b677fc4b6d8744b59a1392b036d606991d6731
jb200957 <t2sudi00@students.oamk.fi>
2023-04-01 20:37:28 +0300

    http_calls

--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,53 @@
-const list = <HTMLUListElement>document.getElementById("todolist") 
-const input = <HTMLInputElement>document.querySelector("#newtodo")
+let BACKEND_ROOT_URL = "http://localhost:5000";
+const list = <HTMLUListElement>document.getElementById("todolist");
+const input = <HTMLInputElement>document.querySelector("#newtodo");
+input.disabled = true;
 
-input.addEventListener("keypress", event => {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        const text = input.value.trim()
-        if (text !== "") {
-            const list_item = document.createElement("li")
-            list_item.setAttribute("class", "list-group-item")
-            list_item.innerHTML = text
-            list.append(list_item)
-            input.value =''
-        }
+fetch(BACKEND_ROOT_URL)
+  .then((response) => response.json())
+  .then(
+    (response) => {
+      response.result.forEach((node) => {
+        renderTask(node.description);
+      });
+      input.disabled = false;
+    },
+    (error) => {
+      alert(error);
     }
-})
+  );
+
+const renderTask = (text) => {
+  const list_item = document.createElement("li");
+  list_item.setAttribute("class", "list-group-item");
+  list_item.innerHTML = text;
+  list.append(list_item);
+};
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const text = input.value.trim();
+    console.log("this is keypress1", text);
+    if (text !== "") {
+      const json = JSON.stringify({ description: text });
+      fetch(BACKEND_ROOT_URL + "/new", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json,
+      })
+        .then((response) => response.json())
+        .then(
+          (response) => {
+            renderTask(text);
+            input.value = "";
+          },
+          (error) => {
+            alert(error);
+          }
+        );
+    }
+  }
+});
